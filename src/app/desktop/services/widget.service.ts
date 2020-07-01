02819970134e3b74d733e21c97d564855a9dbae9
Kuba Matjanowski <jakub.matjanowski@predica.pl>
2020-07-01 07:34:03 +0200

    added template views

--- src/app/desktop/services/widget.service.ts
+++ src/app/desktop/services/widget.service.ts
@@ -11,7 +11,7 @@ export class WidgetService {
     }
 
     public getAllMyWidgets(): Observable<Widget[]> {
-        return this._http.get("http://localhost:7011/widgets/1")
+        return this._http.get("http://localhost:58593/widgets/1")
             .filter(response => response.ok)
             .map(response => response.json())
             .map(json => <Widget[]>json);
